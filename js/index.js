9070edeea4a81bcb6855c858068b1742409b86dc
50m-regent <len.hirata@gmail.com>
2022-11-25 14:17:40 +0900

    sorosoro

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+window.onpageshow = function(event) {
+	if (event.persisted || document.getElementById("menu-btn-check").checked) {
+		 window.location.reload();
+	}
+};
+
 new Vivus("tekkotsu", {
     duration: 100,
     start: "autostart",
